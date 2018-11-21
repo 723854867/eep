c823015946d6ffd2c4f97233c156e93d21a87b10
lynn <723854867@qq.com>
2018-11-21 13:09:06 +0800

    lynn

--- eep-common/src/main/java/org/eep/common/bean/param/DevicesParam.java
+++ eep-common/src/main/java/org/eep/common/bean/param/DevicesParam.java
@@ -1,5 +1,8 @@
 package org.eep.common.bean.param;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Null;
+
 import org.eep.common.bean.enums.WarnLevel;
 import org.rubik.bean.core.param.Param;
 
@@ -12,9 +15,15 @@ public class DevicesParam extends Param {
 
 	private static final long serialVersionUID = -2064641284596312744L;
 
+	@Null
+	private Long min;
+	@Null
+	private Long max;
 	private String id;
 	private String cid;
 	private String name;
+	@Min(1)
+	private Long region;
 	private String code;
 	private String cname;
 	private String model;
